dd8925ec50f414573e7f1a476c20388cee8cde7c
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-09 23:00:37 -0700

    adds routes to homepage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import { ToastContainer } from "react-toastify";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
+  <Router>
     <div className="App">
       <ToastContainer />
-     <h1>hello react</h1>
+    <Routes>
+      <Route path="/" element={<Home />}>Home</Route>
+      <Route path="/login" element={<Login />}>Login</Route>
+      <Route path="/register" element={<Register />}>Signup</Route>
+    </Routes>
     </div>
+  </Router>
   );
 }
 
