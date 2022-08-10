8ab7a302d9d91246836a662fe090ec057c553002
Javier Delgado <99965499+Vanillajaviscript@users.noreply.github.com>
2022-08-10 01:06:10 -0700

    adds navbar links and mdb collapse function

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
   <Router>
     <div className="App">
+      <NavBar />
       <ToastContainer />
     <Routes>
       <Route path="/" element={<Home />}>Home</Route>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import {
+  MDBNavbar,
+  MDBContainer,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBBadge,
+  MDBBtn,
+  MDBNavbarBrand,
+  MDBNavbarLink
+} from "mdb-react-ui-kit";
+import { useSelector, useDispatch } from "react-redux";
+import { setLogout } from "../redux/features/authSlice";
+import { useNavigate } from "react-router-dom";
+
+
+
+const NavBar = () => {
+  const [show, setShow] = useState(false);
+  return (
+   <MDBNavbar fixed="top" expand="lg" style={{backgroundColor: "#6e80a4"}}>
+    <MDBContainer>
+      <MDBNavbarBrand href="/" style={{color: "#0d0f12", fontWeight: "600", fontSize: "22px"}}>
+        Farm Sanctuary
+      </MDBNavbarBrand>
+      <MDBNavbarToggler
+        type="button"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={() => setShow(!show)}
+      >
+      <MDBIcon icon="bars" fas />
+      </MDBNavbarToggler>
+      <MDBCollapse show={show} navbar>
+        <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/">
+              <p className="navbar-text">Home</p>
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/adddog">
+              <p className="navbar-text">Add Dog</p>
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/dashboard">
+              <p className="navbar-text">Dashboard</p>
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/login">
+              <p className="navbar-text">Logout</p>
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+          <MDBNavbarItem>
+            <MDBNavbarLink href="/login">
+              <p className="navbar-text">Login</p>
+            </MDBNavbarLink>
+          </MDBNavbarItem>
+        </MDBNavbarNav>
+      </MDBCollapse>
+    </MDBContainer>
+   </MDBNavbar>
+  )
+}
+export default NavBar
